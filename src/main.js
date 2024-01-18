a04ef66d50b684fcfe2efcca3a93151ba34ae373
pyindent <randall.thomas.sun@gmail.com>
2024-01-18 21:05:08 +0300

    vuetify added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "@/App.vue";
 
 import router from './router'
 import store from './store';
+import vuetify from './lib/vuetify';
 
 
-createApp(App).use(router).use(store).mount("#app");
+createApp(App).use(vuetify).use(router).use(store).mount("#app");
