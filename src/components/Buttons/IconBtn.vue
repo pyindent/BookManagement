40844c7c8ef0b5569f22eba170ff486e59204f15
pyindent <randall.thomas.sun@gmail.com>
2024-01-21 21:50:26 +0300

    icon button complete

--- src/components/Buttons/IconBtn.vue
+++ src/components/Buttons/IconBtn.vue
@@ -1,32 +1,31 @@
 <template>
-    <v-btn :color="color" size="small" class="ma-1" @click="action" icon>
-      <v-tooltip :activator="parent" location="top">
-        {{ tooltip }}
-      </v-tooltip>
-      <v-icon> {{icon}} </v-icon>
-    </v-btn>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      color: {
-        type: String,
-        required: true
-      },
-      tooltip: {
-        type: String,
-        required: true
-      },
-      icon: {
-        type: String,
-        required: true
-      },
-      action: {
-        type: Function,
-        required: true
-      }
-    }
-  }
-  </script>
-  
+  <v-btn :color="color" size="small" class="ma-1" @click="action" icon ref="button">
+    <v-tooltip :activator="$refs.button" location="top">
+      {{ tooltip }}
+    </v-tooltip>
+    <v-icon>{{ icon }}</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      required: true,
+    },
+    tooltip: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    action: {
+      type: Function,
+      required: true,
+    },
+  },
+};
+</script>
